1e391685e3602899d2b4ef23f5604fc3167f8f5f
jojoe77777 <jojoe77777@users.noreply.github.com>
2025-01-12 12:21:50 +1100

    Move cemetery options in json

--- src/main/java/me/contaria/seedqueue/customization/Layout.java
+++ src/main/java/me/contaria/seedqueue/customization/Layout.java
@@ -84,8 +84,8 @@ public class Layout {
                 jsonObject.has("cemetery") ? Group.fromJson(jsonObject.getAsJsonObject("cemetery")) : null,
                 jsonObject.has("replaceLockedInstances") && jsonObject.get("replaceLockedInstances").getAsBoolean(),
                 jsonObject.has("mainFillOrder") ? MainFillOrder.valueOf(jsonObject.get("mainFillOrder").getAsString().toUpperCase(Locale.ROOT)) : MainFillOrder.FORWARD,
-                jsonObject.has("autoOpenCemetery") && jsonObject.get("autoOpenCemetery").getAsBoolean(),
-                jsonObject.has("autoCloseCemetery") && jsonObject.get("autoCloseCemetery").getAsBoolean()
+                jsonObject.has("cemetery") && jsonObject.getAsJsonObject("cemetery").has("defaultOpen") && jsonObject.getAsJsonObject("cemetery").get("defaultOpen").getAsBoolean(),
+                jsonObject.has("cemetery") && jsonObject.getAsJsonObject("cemetery").has("autoClose") && jsonObject.getAsJsonObject("cemetery").get("autoClose").getAsBoolean()
         );
     }
 
